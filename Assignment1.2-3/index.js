09d758d5d0853676e9190dd72b175bd47e2fa933
Ihor Kopach <immortal90@meta.ua>
2024-09-09 20:44:33 +0300

    add Assignment1.2-3

---
+++ Assignment1.2-3/index.js
@@ -0,0 +1,56 @@
+const cities = `48.30,32.16,Кропивницький,200000,
+44.38,34.33,Алушта,31440,
+49.46,30.17,Біла Церква,200131,
+49.54,28.49,Бердичів,87575,#некоммент
+50.45,30.52,Київ,2952301
+49.99,36.23,Харків,1421125
+46.48,30.72,Одеса,1010537
+48.46,35.05,Дніпро,968502
+48.02,37.80,Донецьк,901645
+49.84,24.03,Львів,717273
+47.84,35.14,Запоріжжя,710052
+47.91,33.39,Кривий Ріг,603904
+44.62,33.52,Севастополь,547820
+46.98,31.99,Миколаїв,470011
+47.10,37.54,Маріуполь,425681
+48.57,39.31,Луганськ,397677
+44.95,34.10,Сімферополь,340540
+48.02,37.98,Макіївка,338968
+51.51,31.29,Чернігів,282747
+49.59,34.55,Полтава,279593
+46.64,32.62,Херсон,279131
+49.42,26.99,Хмельницький,274452
+49.44,32.06,Черкаси,269836
+48.29,25.94,Чернівці,264298
+50.25,28.66,Житомир,261624
+50.91,34.80,Суми,256474
+
+#
+46.49,36.58,#Бердянськ,121692,
+49.15,28.41,Вінниця,356665,
+#45.40,34.29,Джанкой,43343,
+
+# в цьому файлі три рядки-коментаря :)`;
+
+let temp;
+let city;
+const processCities = csv => (str => str
+    .replace(/\{([^}]+)}/g, (_, cityName) => (temp = csv.split("\n")
+        .filter(temp => temp.trim() !== '' && !temp.trim().startsWith("#"))
+        .map(city => ({
+            latitude: city.split(',')[0],
+            longitude: city.split(',')[1],
+            city: city.split(',')[2],
+            population: parseInt(city.split(',')[3])
+        }))
+        .sort((a, b) => b.population - a.population)
+        .slice(0, 10)
+        .reduce((accumulator, city, index) => ({
+            ...accumulator,
+            [city.city]: {population: city.population, rating: index + 1}
+        }), {}), city = temp[cityName], city ? `City: ${cityName}, Population: ${city.population}, Rating: ${city.rating}` : `{${cityName}}`)));
+
+
+const replaceCityData = processCities(cities);
+const result = replaceCityData("The top cities are {Київ}, {Харків}, and {Одеса}.");
+console.log(result);
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [Assignment1.1-1](Assignment1.1-1/) - Перше завдання першого рівня https://immortal90.github.io/resume/   
 [Assignment1.1-3](Assignment1.1-3/) - Третє завдання першого рівня https://immortal90.github.io/Leading-of-the-DiGi-mockup/
-[Assignment1.2-3](Assignment1.2-3/) - Третє завдання другого рівня https://immortal90.github.io/Assingment2.2-3/
+[Assignment1.2-3](Assignment1.2-3/) - Третє завдання другогоgit рівня https://immortal90.github.io/Assingment2.2-3/
